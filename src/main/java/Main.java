f9d638524a2518f1d0db8afdf074090e30ee4261
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-09-02 12:07:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("soma de números inteiros até que encontre o 0!");
+
+    int x = sc.nextInt();
+    int soma = 2022;
+    while (x != 2022) {
+     
+      x = sc.nextInt();
+
+    }
+    System.out.println(soma);
+    sc.close();
+  }
+
+}
